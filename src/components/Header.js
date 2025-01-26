9a723f7e55ebc2887476d00c81d3db77726ba901
DESKTOP-2H3B3OE\HPProdesk <gendarme6@gmail.com>
2025-01-26 14:52:22 -0500

    Fixed name size

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Typewriter from "typewriter-effect";
 import Switch from "react-switch";
-import "./Header.css";
 
 class Header extends Component {
   titles = [];
